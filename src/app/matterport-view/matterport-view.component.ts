c5897d7b892e9c4774b871dd84e2d36b3fc62712
Yuma Ito <38903000+yuma-ito-bd@users.noreply.github.com>
2020-12-28 12:20:21 +0900

    コンポーネントが削除されるときにunsubscribeするように修正（メモリリーク対策）

--- src/app/matterport-view/matterport-view.component.ts
+++ src/app/matterport-view/matterport-view.component.ts
@@ -1,17 +1,26 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import {
+    Component,
+    ElementRef,
+    OnDestroy,
+    OnInit,
+    ViewChild,
+} from '@angular/core';
 import { TagId } from '../models/TagId';
 import { MatterportService } from '../services/matterport.service';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'app-matterport-view',
     templateUrl: './matterport-view.component.html',
     styleUrls: ['./matterport-view.component.scss'],
 })
-export class MatterportViewComponent implements OnInit {
+export class MatterportViewComponent implements OnInit, OnDestroy {
     @ViewChild('showCaseIframe', { static: true })
     showCaseElement!: ElementRef<HTMLIFrameElement>;
     selectedTagId?: TagId;
     hoveredTagId?: TagId;
+    private onDestroy$ = new Subject();
 
     constructor(private matterPort: MatterportService) {}
 
@@ -22,11 +31,21 @@ export class MatterportViewComponent implements OnInit {
         });
 
         await this.matterPort.initializeSDK(this.showCaseElement.nativeElement);
-        this.matterPort.listenClickEvent().subscribe((id) => {
-            this.selectedTagId = id;
-        });
-        this.matterPort.listenHoverEvent().subscribe(({ id, hovering }) => {
-            this.hoveredTagId = hovering ? id : undefined;
-        });
+        this.matterPort
+            .listenClickEvent()
+            .pipe(takeUntil(this.onDestroy$))
+            .subscribe((id) => {
+                this.selectedTagId = id;
+            });
+        this.matterPort
+            .listenHoverEvent()
+            .pipe(takeUntil(this.onDestroy$))
+            .subscribe(({ id, hovering }) => {
+                this.hoveredTagId = hovering ? id : undefined;
+            });
+    }
+
+    ngOnDestroy(): void {
+        this.onDestroy$.next();
     }
 }
